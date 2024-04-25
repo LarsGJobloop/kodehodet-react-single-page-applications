1717e741e8f43ef03133e00f2c565e09e29b03e8
larsgjobloop <lars.gunnar@jobloop.no>
2024-04-25 13:08:48 +0000

    add styling to layout

--- react-spa/src/routes/layoutRoot.jsx
+++ react-spa/src/routes/layoutRoot.jsx
@@ -1,34 +1,51 @@
 import { Link, Outlet } from "react-router-dom"
+import style from './layoutRoot.module.css'
 
 export function LayoutRoot() {
   return (
-    <div>
-      <header>
-        <p>LogoPlaceholder</p>
+    <div className={style["layout"]}>
+      <header className={["flex justify-between items-center px-md py-sm bg-white", style["navbar"]].join(" ")}>
+        <Link to="/" className="hover-scale">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width={30} height={30}>
+            <circle cx="12" cy="12" r="11" fill="orange" stroke="none" />
+            <path stroke="purple" strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+          </svg>
+        </Link>
 
         <nav>
-          <ul>
-            <li>
+          <ul className="flex gap-md">
+            <li className="hover-scale">
               <Link to="/">Home</Link>
             </li>
-            <li>
+            <li className="hover-scale">
               <Link to="/about">About</Link>
             </li>
-            <li>
+            <li className="hover-scale">
               <Link to="/contact">Contact</Link>
             </li>
-            <li>
+            <li className="hover-scale">
               <Link to="/projects">Projects</Link>
             </li>
           </ul>
         </nav>
       </header>
 
-      <Outlet />
+      <div>
+        <Outlet />
+      </div>
 
-      <footer>
-        <p>Kodehode 2024</p>
-        <a href="https://github.com" target="_blank" referrerPolicy="no-referrer">Source Code</a>
+      <footer className={["px-md py-md flex justify-between", style["footer"]].join(" ")}>
+        <div className="flex flex-col gap-md">
+          <p>Kodehode 2024</p>
+          <a href="https://github.com" target="_blank" referrerPolicy="no-referrer">Source Code</a>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <a className="hover-scale" href="https://github.com/larsgjobloop" target="_blank" referrerPolicy="no-referrer">
+            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="3em" width="3em" xmlns="http://www.w3.org/2000/svg"><path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM277.3 415.7c-8.4 1.5-11.5-3.7-11.5-8 0-5.4.2-33 .2-55.3 0-15.6-5.2-25.5-11.3-30.7 37-4.1 76-9.2 76-73.1 0-18.2-6.5-27.3-17.1-39 1.7-4.3 7.4-22-1.7-45-13.9-4.3-45.7 17.9-45.7 17.9-13.2-3.7-27.5-5.6-41.6-5.6-14.1 0-28.4 1.9-41.6 5.6 0 0-31.8-22.2-45.7-17.9-9.1 22.9-3.5 40.6-1.7 45-10.6 11.7-15.6 20.8-15.6 39 0 63.6 37.3 69 74.3 73.1-4.8 4.3-9.1 11.7-10.6 22.3-9.5 4.3-33.8 11.7-48.3-13.9-9.1-15.8-25.5-17.1-25.5-17.1-16.2-.2-1.1 10.2-1.1 10.2 10.8 5 18.4 24.2 18.4 24.2 9.7 29.7 56.1 19.7 56.1 19.7 0 13.9.2 36.5.2 40.6 0 4.3-3 9.5-11.5 8-66-22.1-112.2-84.9-112.2-158.3 0-91.8 70.2-161.5 162-161.5S388 165.6 388 257.4c.1 73.4-44.7 136.3-110.7 158.3zm-98.1-61.1c-1.9.4-3.7-.4-3.9-1.7-.2-1.5 1.1-2.8 3-3.2 1.9-.2 3.7.6 3.9 1.9.3 1.3-1 2.6-3 3zm-9.5-.9c0 1.3-1.5 2.4-3.5 2.4-2.2.2-3.7-.9-3.7-2.4 0-1.3 1.5-2.4 3.5-2.4 1.9-.2 3.7.9 3.7 2.4zm-13.7-1.1c-.4 1.3-2.4 1.9-4.1 1.3-1.9-.4-3.2-1.9-2.8-3.2.4-1.3 2.4-1.9 4.1-1.5 2 .6 3.3 2.1 2.8 3.4zm-12.3-5.4c-.9 1.1-2.8.9-4.3-.6-1.5-1.3-1.9-3.2-.9-4.1.9-1.1 2.8-.9 4.3.6 1.3 1.3 1.8 3.3.9 4.1zm-9.1-9.1c-.9.6-2.6 0-3.7-1.5s-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3 1.1 1.5 1.1 3.3 0 4.1zm-6.5-9.7c-.9.9-2.4.4-3.5-.6-1.1-1.3-1.3-2.8-.4-3.5.9-.9 2.4-.4 3.5.6 1.1 1.3 1.3 2.8.4 3.5zm-6.7-7.4c-.4.9-1.7 1.1-2.8.4-1.3-.6-1.9-1.7-1.5-2.6.4-.6 1.5-.9 2.8-.4 1.3.7 1.9 1.8 1.5 2.6z"></path></svg>
+          </a>
+          <p className="italianno">Lars Gunnar</p>
+        </div>
       </footer>
     </div>
   )
